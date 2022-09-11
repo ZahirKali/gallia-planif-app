151d7a8c89302e3330fdf31da7e7465bdbcb7fa7
Zahir KALI <sofiane.kali@gmail.com>
2022-09-12 00:08:51 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDrawerMode } from '@angular/material/sidenav';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,6 +10,9 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'gallia-planif-app';
+
+  mode = new FormControl('over' as MatDrawerMode);
+  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
 
   constructor(private titleService:Title) {
     this.titleService.setTitle("Gallia - Planification");
